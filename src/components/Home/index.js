ce17851cc3fbf910126cb7dd7430566fcb760810
Ciber-marcelo <marcelones125@gmail.com>
2022-09-21 12:25:35 -0300

    serachbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './style.css'
 
 export const Home = () => {
   const [personagens, setPersonagens] = useState([]);
+  const [pesquisa, setPesquisa] = useState('');
 
   const api = axios.create({
     baseURL: 'https://pokeapi.co/api/v2/'
@@ -35,25 +36,45 @@ export const Home = () => {
     const response = await api.get(url)
     const { id, types, sprites } = response.data;
     return {
-       id, types, sprites
+      id, types, sprites
     }
- }
+  }
+
+  const pesquisar = (e) => {
+    setPesquisa(e.target.value)
+    console.log(pesquisa, "t");
+  }
 
   return (
     <div>
+      <input
+        className="search"
+        placeholder="Digite um nome"
+        onChange={pesquisar}
+      />
       {
         personagens.map((personagem) =>
-          <ul>
-            <li>
-              <img src={personagem.sprites.front_default}/>
-            </li>
-            <li>{personagem.name}</li>
-            <li>{personagem.id}</li>
+          <>
             {
-              personagem.types.map(obj =>
-                <li>{obj.type.name}</li>
-            )}
-          </ul>
+              pesquisa.toUpperCase() === '' || personagem.name.toUpperCase().includes(pesquisa.toUpperCase())
+                ?
+                <ul>
+                  <li>
+                    <img src={personagem.sprites.front_default} />
+                  </li>
+                  <li>{personagem.name}</li>
+                  <li>{personagem.id}</li>
+                  {
+                    personagem.types.map(obj =>
+                      <li>{obj.type.name}</li>
+                    )}
+                </ul>
+                :
+                null
+            }
+
+
+          </>
         )
       }
     </div >
