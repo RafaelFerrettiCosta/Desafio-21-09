9c107a95a1c4bca655a8c996751029481a519f64
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-09-21 09:15:28 -0300

    commit inicial

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,6 @@
+import Home from '../screens/Home';
 import './style.css'
 
 export const App = () => {
-  return (
-    <div>
-      <h1 className="globalContainer">Boa sorte, candidato!</h1>
-    </div>
-  );
+  return <Home />;
 }
