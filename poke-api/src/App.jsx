4ce5f80846466543d82060dfcba34df15406605c
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-09-21 11:23:01 -0300

    feat(app): import styled-components & get pokemos

--- poke-api/src/App.jsx
+++ poke-api/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect } from 'react';
+import { PokemonInfos } from './api/pokemon'
+import { usePokemon } from './context/pokemonInfos';
+import * as Style from './style'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {pokemons, setPokemons} = usePokemon();
+
+  const resultsPokemon = async () => {
+    const payload = await PokemonInfos.GetAll();
+
+    if (payload) {
+      setPokemons(payload.data.results);
+    }
+    else {
+      console.log("erro")
+    }
+  }
+
+  useEffect(() => {
+    resultsPokemon();
+  }, []);
+  console.log(pokemons)
 
   return (
-    <div className="App">
-      
-    </div>
+    <Style.Page>
+      <Style.Infos>
+        {pokemons.map((pokemon, index) => (
+          <Style.Infos key={index}>
+            <p>{pokemon.name}</p>
+          </Style.Infos>
+        ))}
+      </Style.Infos>
+    </Style.Page>
   )
 }
 
