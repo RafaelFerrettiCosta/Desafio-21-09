c535454838ba5ad482c227d694a119232a718f34
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-09-21 11:25:38 -0300

    feat: implement conections and styles

---
+++ poke-api/src/api/pokemon.js
@@ -0,0 +1,22 @@
+import api from "./connection";
+
+export const PokemonInfos = {
+  GetAll: async () => {
+    try {
+      const res = await api.get("/pokemon/");
+      return res;
+    } catch (error) {
+      console.log({ message: `Error` + error });
+      throw new Error(`Erro` + error);
+    }
+  },
+
+  GetById: async (id) => {
+    try {
+        const res = await api.get(`/pokemon/`)
+    }
+    catch(error) {
+
+    }
+  }
+};
---
+++ poke-api/src/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Page = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Infos = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: .6rem;
+`
